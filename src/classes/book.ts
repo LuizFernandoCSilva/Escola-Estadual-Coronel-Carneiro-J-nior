ce0e8e921b4978debe5737d378ce3ef934760e78
Luiz F C Silva <luiz.silva@sharkdev.com.br>
2025-04-10 21:34:52 -0300

    feat: Implementação da classes book

---
+++ src/classes/book.ts
@@ -0,0 +1,29 @@
+import { IBook } from "../interface/book";
+import { ObjectRaw } from "./objectRaw";
+
+export class Book extends ObjectRaw implements IBook {
+    title: string;
+    autor: string;
+    anoPublicacao: Date;
+    isbn: string;
+    editora: string;
+    descricao: string;
+    categoriaId: number;
+    libraryId: number;
+    quantity: number;
+    status: string;
+
+    constructor(book: IBook) {
+        super(book);
+        this.title = book.title;
+        this.autor = book.autor;
+        this.anoPublicacao = book.anoPublicacao;
+        this.isbn = book.isbn;
+        this.editora = book.editora;
+        this.descricao = book.descricao;
+        this.categoriaId = book.categoriaId;
+        this.libraryId = book.libraryId;
+        this.quantity = book.quantity;
+        this.status = book.status;
+    }
+}
