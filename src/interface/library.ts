32b768215e6587a6e30e868a490e6b4ff79cae86
Luiz F C Silva <luiz.silva@sharkdev.com.br>
2025-04-10 21:28:29 -0300

    feat: Implementação da classes library

---
+++ src/interface/library.ts
@@ -0,0 +1,11 @@
+import { IObjectRaw } from "./base"
+
+export type ILibraryRaw = {
+    name: string
+    quantityBooks: number
+    rows: number
+    shelves: number
+}
+
+
+export type ILibrary = ILibraryRaw & IObjectRaw
